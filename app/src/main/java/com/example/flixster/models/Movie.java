dd5957fe8259f2a4b88810db7aaa8e5e1ef1f009
JOSEAROSEMENA <77999340+JOSEAROSEMENA@users.noreply.github.com>
2021-12-03 17:39:17 -0500

    Flix Part 2
    Added implementation for:
    
    - Exposing movie details including ratings with rating bar, and synopsis
    
    - Youtube API functionality to showcase movie trailers via youtube
    
    - udated readme

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,16 +3,41 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    // Empty constructor needed by the Parceler library
+    public Movie(){}
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path");
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
+    }
+
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for(int i = 0; i < movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
 
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
@@ -30,18 +55,11 @@ public class Movie {
         return overview;
     }
 
-    public Movie(JSONObject jsonObject) throws JSONException {
-        backdropPath = jsonObject.getString("backdrop_path");
-        posterPath = jsonObject.getString("poster_path");
-        title = jsonObject.getString("title");
-        overview = jsonObject.getString("overview");
+    public double getRating() {
+        return rating;
     }
 
-    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
-        List<Movie> movies = new ArrayList<>();
-        for(int i = 0; i < movieJsonArray.length(); i++){
-            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
-        }
-        return movies;
+    public int getMovieId() {
+        return movieId;
     }
 }
